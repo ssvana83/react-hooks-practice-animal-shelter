70f2f3304d63d6ee705e0c71cc8eaa399c218dbd
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-01-09 13:13:39 -0500

    commit tree f47cb4d97ba8ddb57093bf5b3159c4d7d5a439b4

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,36 @@ import React, { useState } from "react";
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
-function App() {
+
+function App({ onChange }) {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  function handleChangeType(type) {
+    setFilters({type})
+  }
+
+  function handleFindPetsClick() {
+    let endpoint = 'http://localhost:3001/pets'
+    if(filters.type !== 'all') {
+      endpoint += `?type=${filters.type}`
+    }
+    fetch(endpoint)
+      .then(resp => resp.json())
+      .then(setPets)
+  }
+
+  function handleAdoptPet(id) {
+    // console.log('id: ', id);
+    const updatedPets = pets.map(pet => {
+      if(pet.id === id){
+        return {...pet, isAdopted: true}
+      } else {
+        return pet
+      }
+    })
+    setPets(updatedPets)
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +42,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+              onFindPetsClick={handleFindPetsClick} 
+              onChangeType={handleChangeType} 
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
           </div>
         </div>
       </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Filters() {
+
+function Filters({ onFindPetsClick, onChangeType }) {
+  const [findPets, setFindPets] = useState([])
+
+function handleChange(e) {
+  onChangeType(e.target.value)
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:3001/pets')
+      .then((resp) => resp.json())
+      .then(data => setFindPets(data))
+  },[])
+
+  
+
   return (
     <div className="ui form">
       <h3>Animal type</h3>
       <div className="field">
-        <select name="type" id="type" aria-label="type">
+        <select onChange={handleChange} name="type" id="type" aria-label="type">
           <option value="all">All</option>
           <option value="cat">Cats</option>
           <option value="dog">Dogs</option>
@@ -14,7 +29,7 @@ function Filters() {
       </div>
 
       <div className="field">
-        <button className="ui secondary button">Find pets</button>
+        <button onClick={onFindPetsClick} className="ui secondary button">Find pets</button>
       </div>
     </div>
   );
